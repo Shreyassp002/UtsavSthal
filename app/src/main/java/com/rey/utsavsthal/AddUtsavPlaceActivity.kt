b3340afbe7066411701e9a3782062813b34aa9d4
Shreyas Patil <shreyassp002@gmail.com>
2024-03-21 01:23:50 +0530

    DatePickerDialog Implemented

--- app/src/main/java/com/rey/utsavsthal/AddUtsavPlaceActivity.kt
+++ app/src/main/java/com/rey/utsavsthal/AddUtsavPlaceActivity.kt
@@ -1,21 +1,63 @@
 package com.rey.utsavsthal
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class AddUtsavPlaceActivity : AppCompatActivity() {
+class AddUtsavPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var toolbar_add_place: Toolbar
+    private lateinit var et_date: EditText
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_utsav_place)
 
-        val toolbar_add_place = findViewById<Toolbar>(R.id.toolbar_add_place)
+        toolbar_add_place = findViewById(R.id.toolbar_add_place)
+        et_date = findViewById(R.id.et_date)
+
         setSupportActionBar(toolbar_add_place)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbar_add_place.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
+        dateSetListener = DatePickerDialog.OnDateSetListener{
+                _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
 
+        et_date.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.et_date ->{
+                DatePickerDialog(this@AddUtsavPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        et_date.setText(sdf.format(cal.time).toString())
     }
 }
