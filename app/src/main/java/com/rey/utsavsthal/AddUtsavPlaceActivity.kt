710f3aeda6e7548f0fe453fbd401510647c905f7
Shreyas Patil <shreyassp002@gmail.com>
2024-03-30 02:31:05 +0530

    Implemented permissions using Dexter API

--- app/src/main/java/com/rey/utsavsthal/AddUtsavPlaceActivity.kt
+++ app/src/main/java/com/rey/utsavsthal/AddUtsavPlaceActivity.kt
@@ -1,20 +1,34 @@
 package com.rey.utsavsthal
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
+
 
 class AddUtsavPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var toolbar_add_place: Toolbar
     private lateinit var et_date: EditText
+    private lateinit var tv_add_image: TextView
 
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: OnDateSetListener
@@ -25,6 +39,7 @@ class AddUtsavPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
         toolbar_add_place = findViewById(R.id.toolbar_add_place)
         et_date = findViewById(R.id.et_date)
+        tv_add_image = findViewById(R.id.tv_add_image)
 
         setSupportActionBar(toolbar_add_place)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -41,6 +56,7 @@ class AddUtsavPlaceActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -52,7 +68,62 @@ class AddUtsavPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)).show()
             }
+
+            R.id.tv_add_image ->{
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf("Choose From Gallery", "Capture From Camera")
+                pictureDialog.setItems(pictureDialogItems){
+                    _, which ->
+                    when(which){
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(
+                            this@AddUtsavPlaceActivity,
+                            "Implementing Soon", Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
+    }
+
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {if (report!!.areAllPermissionsGranted()){
+                    Toast.makeText(this@AddUtsavPlaceActivity,
+                        "Storage READ/WRITE permissions are granted, Now you can select an image from GALLERY",
+                        Toast.LENGTH_LONG).show()
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest?>,
+                    token: PermissionToken
+                ) {showRationaleDialogForPermissions()} }).onSameThread().check()
+    }
+
+    private fun showRationaleDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage(
+            "It looks like you have turned off permissions required for this feature. " +
+                    "It can be enabled under the Applications Settings"
+        ).setPositiveButton("GO TO SETTINGS")
+        { _,_ ->
+            try {
+                val intent = Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }catch (e: ActivityNotFoundException){
+                e.printStackTrace()
+            }
+        }.setNegativeButton("Cancel"){ dialog, _ ->
+            dialog.dismiss()
+        }.show()
     }
 
     private fun updateDateInView(){
